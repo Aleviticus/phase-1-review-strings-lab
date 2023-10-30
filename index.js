d4dafc4a9c097ca2051842b1aff7da5c2d82502a
Aaron Levy <aaronlevypc21@gmail.com>
2023-10-30 12:45:15 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Write your code in this file!
+const currentUser = "Aaron"
+const welcomeMessage = 'Welcome to Flatbook, currentUser to contain Aaron!'
+const excitedWelcomeMessage = 'WELCOME TO FLATBOOK, currentUser to contain AARON!'
+const shortGreeting = 'Welcome, currentUser to contain first initial A!' 
