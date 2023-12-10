3656a78b263a8a9c4c91068f9a3083aee463bc8f
Aaron Levy <aaronlevypc21@gmail.com>
2023-12-10 00:48:05 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
-// Write your code in this file!
-const currentUser = "Aaron"
-const welcomeMessage = 'Welcome to Flatbook, currentUser to contain Aaron!'
-const excitedWelcomeMessage = 'WELCOME TO FLATBOOK, currentUser to contain AARON!'
-const shortGreeting = 'Welcome, currentUser to contain first initial A!' 
+const currentUser = 'Grace Hopper';
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
